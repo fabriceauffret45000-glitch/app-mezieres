90025d7cccbaa27da0b51260b8fda6b55d8631c7
mairie-mezieres <fabrice.auffret45000@gmail.com>
2026-03-26 18:00:22 +0100

    Update cache name and modify service worker logic

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
-const CACHE_NAME = "mezieres-v2";
-
-const URLS_TO_CACHE = [
-  "./",
-  "./index.html"
-];
+const CACHE_NAME = "app-mezieres-v3";
 
 self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(URLS_TO_CACHE))
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => caches.delete(key)))
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
